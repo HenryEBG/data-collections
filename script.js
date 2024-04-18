23ebb8b0f02112546d274758f3ec30f845703d7c
HenryEBG <berganza386@gmail.com>
2024-04-17 22:27:29 -0400

    giving solution to exercise No. 5

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
  * Data Structures 
  */
 
-/**
- * Exercise No. 1
- */
+console.log(`*************************`)
+console.log(`****  Exercise No. 1 ****`)
+console.log(`*************************`)
 
 //initial string
 let strVar="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
-let lines=[]
-let cells=[]
+let lines=[] //array to save the lines
+let cells=[] //array to temporary save an array
+
 //separate the string in an array.  
 lines=strVar.split("\n")
 
-for(i=0;i<lines.length;i++){
-  //separate every line in cells and printed
+//separate every line in cells and printed
+for(i=0;i<lines.length;i++){  
   cells=lines[i].split(",")
   console.log(` ${cells[0]} ${cells[1]} ${cells[2]} ${cells[3]}`)
 }
@@ -24,26 +25,32 @@ for(i=0;i<lines.length;i++){
  * Exercise No. 2
  */
 
-//initial string
+console.log(`*************************`)
+console.log(`****  Exercise No. 2 ****`)
+console.log(`*************************`)
+
+
 //initial string
 strVar="ID,Name,Occupation,Age\n42,Bruce,Knight,41,otherthing\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
-//initialize rows array
-lines=[]
-//initialize columns array
-cells=[]
-//initialize 2D array
-savedArray=[]
-//initialize 
-let columnQuantity
-//separate the string in an array.  
+
+
+lines=[]//initialize rows array
+cells=[]//initialize columns array
+savedArray=[]//initialize 2D array
+
+let columnQuantity//variable to saved the number of columns
+
+//separate the string in arrays.  
 lines=strVar.split("\n")
-//first task.  Know how many columns are in the title row
+
+//Task #1.  Know how many columns are in the title row
 cells=lines[0].split(",")
-console.log(`We have a quantity of: ${cells}`)
+
 //we check the quantity of columns the title row has
 columnQuantity=cells.length
-console.log(columnQuantity)
+console.log(`We have a quantity of: ${columnQuantity}`)
 
+//this loop will work with any number of columns.
 for(i=0;i<lines.length;i++){
   cells=lines[i].split(",")
   //if for some reason the lines after the title has more columns we can truncate the array
@@ -59,6 +66,10 @@ console.log(savedArray)
 /**
  * Exercise No. 3
  */
+
+console.log(`*************************`)
+console.log(`****  Exercise No. 3 ****`)
+console.log(`*************************`)
 
 //I will use the savedArray from exercise 2
 //console.log(savedArray.length)
@@ -78,6 +89,10 @@ console.log(objectArray)
  * Exercise No. 4
  */
 
+console.log(`*************************`)
+console.log(`****  Exercise No. 4 ****`)
+console.log(`*************************`)
+
 objectArray.pop()
 objectArray.splice(1,0,{ id: "48", name: "Barry", occupation: "Runner", age: "25" })
 objectArray.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
@@ -90,6 +105,38 @@ for(i=0;i<objectArray.length;i++){
 }
 averageAge=averageAge/objectArray.length
 console.log(`The average age is : ${averageAge} `)
+
+
+/**
+ * Exercise No. 5
+ */
+console.log(`*************************`)
+console.log(`****  Exercise No. 5 ****`)
+console.log(`*************************`)
+
+//let initialFormat=objectArray.toString
+//objectArray.entries
+
+//first solution
+//using Object.keys we can get the keys to be the first part of our string
+let stringReverse=''
+
+//crear los titulos
+stringReverse=Object.keys(objectArray[0]).toString()
+//recorrer el array de objetos y tomar los datos
+for(i=1; i<objectArray.length;i++){
+  stringReverse+='\\n'
+  stringReverse+=Object.values(objectArray[i]).toString()
+}
+
+console.log(stringReverse)
+
+
+
+
+
+
+
 
 
 
